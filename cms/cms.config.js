50a5db5ffb6e3a2dbaf85b4e5abc2a7e0b18d6b7
thinmelon <flowerinhouse@163.com>
2019-04-09 17:48:09 +0800

    Initial commit

---
+++ cms/cms.config.js
@@ -0,0 +1,163 @@
+/**
+ * 参数配置 *
+ */
+
+var cmsConfig = {
+    serverUrl: 'http://10.184.255.10:8080/manage/web/',		    //  给机顶盒用的
+    imgUrl: 'http://10.184.255.10:8080/manage/',
+    backUrl: '',
+    index_back_url: '',
+    environment: 'DEBUG',
+    // environment: 'PRODUCT',
+
+    /**
+     * 理论宣讲
+     */
+    indexResourceIdArray: [
+        {title: '理论宣讲', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '中下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     * 社会宣传
+     */
+    societyResourceIdArray: [
+        {title: '社会宣传', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左侧海报', resourceId: '941'},
+        {title: '右侧海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     * 文化惠民
+     */
+    peopleResourceIdArray: [
+        {title: '文化惠民', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     * 教育服务
+     */
+    educationResourceIdArray: [
+        {title: '教育服务', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '中下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+
+    /**
+     * 科技与科普
+     */
+    technologyResourceIdArray: [
+        {title: '科技与科普', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '中下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     * 体育健身
+     */
+    sportsResourceIdArray: [
+        {title: '体育健身', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     * 公民道德
+     */
+    moralResourceIdArray: [
+        {title: '公民道德', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    /**
+     *  乡风文明
+     */
+    cultureResourceIdArray: [
+        {title: '乡风文明', resourceId: ''},
+        // ---------------  菜单  ---------------  //
+
+
+        // ---------------  海报（播放视频）  ---------------  //
+        {title: '左上海报', resourceId: '941'},
+        {title: '右上海报', resourceId: '943'},
+        {title: '左下海报', resourceId: '943'},
+        {title: '右下海报', resourceId: '943'},
+
+        // ---------------  列表页  ---------------  //
+        {title: '列表', resourceId: '942'}
+    ],
+
+    operator: '',
+    weather: '',
+    temperature: '',
+    windScale: ''
+};
---
+++ wrapper/bar.wrapper.js
@@ -0,0 +1,64 @@
+function BarComponent() {
+    BarModule.call(this);
+
+    this.barItemArray = [
+        {
+            title: '理论宣讲',
+            resourceId: 0,
+            url: 'index.html',
+            left: 15,
+            width: 132
+        },
+        {
+            title: '社会宣传',
+            resourceId: 0,
+            url: 'society.html',
+            left: 160,
+            width: 140
+        },
+        {
+            title: '文化惠民',
+            resourceId: 0,
+            url: 'people.html',
+            left: 305,
+            width: 140
+        },
+        {
+            title: '教育服务',
+            resourceId: 0,
+            url: 'education.html',
+            left: 448,
+            width: 140
+        },
+        {
+            title: '科技与科普',
+            resourceId: 0,
+            url: 'technology.html',
+            left: 585,
+            width: 150
+        },
+        {
+            title: '体育健身',
+            resourceId: 0,
+            url: 'sports.html',
+            left: 740,
+            width: 140
+        },
+        {
+            title: '公民道德',
+            resourceId: 0,
+            url: 'moral.html',
+            left: 880,
+            width: 140
+        },
+        {
+            title: '乡风文明',
+            resourceId: 0,
+            url: 'culture.html',
+            left: 1025,
+            width: 140
+        }
+    ];
+
+
+}
